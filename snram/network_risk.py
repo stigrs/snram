39bfadab62c07d0b18b33b329a741eed75f1e619
Sellevaag, Stig Rune <stig-rune.sellevag@ffi.no>
2020-08-01 21:13:43 +0200

    More coding

--- snram/network_risk.py
+++ snram/network_risk.py
@@ -6,7 +6,6 @@
 """Provides a network risk model."""
 
 import numpy as np
-import networkx as nx
 from snram.topology import NetworkTopology
 
 
@@ -68,8 +67,8 @@ class NetworkRisk:
 
     def _compute_lambda_threat(self):
         """Compute Lagrange multiplier for threat allocation."""
-        vuln = self.vulnerability(self._prevent_cost)
-        cons = self.consequence(self._response_cost)
+        vuln = self.vulnerability()
+        cons = self.consequence()
         d_node = self._topology.node_degree_centrality()
         d_arc = self._topology.arc_betweenness_centrality()
         a_node = self._alpha(self._nodes)
@@ -91,63 +90,67 @@ class NetworkRisk:
             a_inv += 1.0 / a_arc[i]
 
         return (t_sum - self._attack_budget) / a_inv
-    
+
     def _compute_lambda_prevent(self):
         """Compute Lagrange multiplier for prevent allocation."""
-        threat = self.threat(self._attack_cost)
-        cons = self.consequence(self._response_cost)
+        threat = self.threat()
+        cons = self.consequence()
         d_node = self._topology.node_degree_centrality()
         d_arc = self._topology.arc_betweenness_centrality()
         g_node = self._gamma(self._nodes)
         g_arc = self._gamma(self._arcs)
-        
+
         n = len(self._nodes)
         m = len(self._arcs)
-        
+
         p_sum = 0.0
         for i in range(n):
-            p_sum += np.log(d_node[i] * threat[i] * self._nodes["v_init"][i] * cons[i]) / g_node[i]
+            p_sum += np.log(d_node[i] * threat[i] *
+                            self._nodes["v_init"][i] * cons[i]) / g_node[i]
         for i in range(m):
-            p_sum += np.log(d_arc[i] * threat[n + i] * self._arcs["v_init"][i] * cons[n + i]) / g_arc[i]
-            
+            p_sum += np.log(d_arc[i] * threat[n + i] *
+                            self._arcs["v_init"][i] * cons[n + i]) / g_arc[i]
+
         g_inv = 0.0
         for i in range(n):
             g_inv += 1.0 / g_node[i]
         for i in range(m):
             g_inv += 1.0 / g_arc[i]
-            
+
         return (p_sum - self._prevent_budget) / g_inv
-    
+
     def _compute_lambda_response(self):
         """Compute Lagrange multiplier for response allocation."""
-        threat = self.threat(self._attack_cost)
-        vuln = self.vulnerability(self._prevent_cost)
+        threat = self.threat()
+        vuln = self.vulnerability()
         d_node = self._topology.node_degree_centrality()
         d_arc = self._topology.arc_betweenness_centrality()
         b_node = self._beta(self._nodes)
         b_arc = self._beta(self._arcs)
-        
+
         n = len(self._nodes)
         m = len(self._arcs)
-        
+
         r_sum = 0.0
         for i in range(n):
-            r_sum += np.log(d_node[i] * threat[i] * vuln[i] * self._nodes["c_init"][i]) / b_node[i]
+            r_sum += np.log(d_node[i] * threat[i] * vuln[i]
+                            * self._nodes["c_init"][i]) / b_node[i]
         for i in range(m):
-            r_sum += np.log(d_arc[i] * threat[n + i] * vuln[n + i] * self._arcs["c_init"][i]) / b_arc[i]
-            
+            r_sum += np.log(d_arc[i] * threat[n + i] * vuln[n + i]
+                            * self._arcs["c_init"][i]) / b_arc[i]
+
         b_inv = 0.0
         for i in range(n):
             b_inv += 1.0 / b_node[i]
         for i in range(m):
             b_inv += 1.0 / b_arc[i]
-            
+
         return (r_sum - self._response_budget) / b_inv
 
     def _compute_attack_cost(self):
         """Compute attack allocation cost."""
-        vuln = self.vulnerability(self._prevent_cost)
-        cons = self.consequence(self._response_cost)
+        vuln = self.vulnerability()
+        cons = self.consequence()
         d_node = self._topology.node_degree_centrality()
         d_arc = self._topology.arc_betweenness_centrality()
         a_node = self._alpha(self._nodes)
@@ -173,101 +176,121 @@ class NetworkRisk:
 
     def _compute_prevent_cost(self):
         """Compute prevent allocation cost."""
-        threat = self.threat(self._attack_cost)
-        cons = self.consequence(self._response_cost)
+        threat = self.threat()
+        cons = self.consequence()
         d_node = self._topology.node_degree_centrality()
         d_arc = self._topology.arc_betweenness_centrality()
         g_node = self._gamma(self._nodes)
         g_arc = self._gamma(self._arcs)
-        
+
         n = len(self._nodes)
         m = len(self._arcs)
-        
+
         lambda_p = self._compute_lambda_prevent()
-        
+
         for i in range(n):
-            self._prevent_cost[i] = (np.log(d_node[i] * threat[i] * self._nodes["v_init"][i] * cons[i]) - lambda_p) / g_node[i]
+            self._prevent_cost[i] = (np.log(
+                d_node[i] * threat[i] * self._nodes["v_init"][i] * cons[i]) - lambda_p) / g_node[i]
             if self._prevent_cost[i] < 0.0:
                 self._prevent_cost[i] = 0.0
         for i in range(m):
-            self._prevent_cost[n + i] = (np.log(d_arc[i] * threat[n + i] * self._arcs["v_init"][i] * cons[n + i]) - lambda_p) / g_arc[i]
+            self._prevent_cost[n + i] = (np.log(d_arc[i] * threat[n + i] *
+                                                self._arcs["v_init"][i] * cons[n + i]) - lambda_p) / g_arc[i]
             if self._prevent_cost[n + i] < 0.0:
                 self._prevent_cost[n + i] = 0.0
-            
+
         return self._prevent_cost
-    
+
     def _compute_response_cost(self):
         """Compute response allocation cost."""
-        threat = self.threat(self._attack_cost)
-        vuln = self.vulnerability(self._prevent_cost)
+        threat = self.threat()
+        vuln = self.vulnerability()
         d_node = self._topology.node_degree_centrality()
         d_arc = self._topology.arc_betweenness_centrality()
         b_node = self._beta(self._nodes)
         b_arc = self._beta(self._arcs)
-        
+
         n = len(self._nodes)
         m = len(self._arcs)
-        
+
         lambda_r = self._compute_lambda_response()
-        
+
         for i in range(n):
-            self._response_cost[i] = (np.log(d_node[i] * threat[i] * vuln[i] * self._nodes["c_init"][i]) - lambda_r) / b_node[i]
+            self._response_cost[i] = (np.log(
+                d_node[i] * threat[i] * vuln[i] * self._nodes["c_init"][i]) - lambda_r) / b_node[i]
             if self._response_cost[i] < 0.0:
-                self._reaponse_cost[i] = 0.0
+                self._response_cost[i] = 0.0
         for i in range(m):
-            self._response_cost[n + i] = (np.log(d_arc[i] * threat[n + i] * vuln[n + i] * self._arcs["c_init"][i]) - lambda_r) / b_arc[i]
+            self._response_cost[n + i] = (np.log(d_arc[i] * threat[n + i]
+                                                 * vuln[n + i] * self._arcs["c_init"][i]) - lambda_r) / b_arc[i]
             if self._response_cost[n + i] < 0.0:
                 self._response_cost[n + i] = 0.0
-            
-        return self._response_cost               
-    
-    def threat(self, attack_cost):
+
+        return self._response_cost
+
+    def threat(self):
         """Compute threat function defining the probability of an attack."""
         threat = []
         for i, ai in enumerate(self._alpha(self._nodes)):
             if self._nodes["attackable"][i] == 1:
-                threat.append(1.0 - np.exp(-ai * attack_cost[i]))
+                threat.append(1.0 - np.exp(-ai * self._attack_cost[i]))
             else:
                 threat.append(0)
         for i, ai in enumerate(self._alpha(self._arcs)):
             if self._arcs["attackable"][i] == 1:
                 threat.append(
-                    1.0 - np.exp(-ai * attack_cost[len(self._nodes) + i]))
+                    1.0 - np.exp(-ai * self._attack_cost[len(self._nodes) + i]))
             else:
                 threat.append(0)
         return threat
 
-    def vulnerability(self, prevent_cost):
-        """Compute vulnerability function defining the probability of 
+    def vulnerability(self):
+        """Compute vulnerability function defining the probability of
         destruction if attacked."""
         vuln = []
         for i, gi in enumerate(self._gamma(self._nodes)):
             if self._nodes["attackable"][i] == 1:
-                vuln.append(self._nodes=["v_init"][i] * np.exp(-gi * prevent_cost[i]))
+                vuln.append(self._nodes["v_init"][i] *
+                            np.exp(-gi * self._prevent_cost[i]))
             else:
                 vuln.append(0)
         for i, gi in enumerate(self._gamma(self._arcs)):
             if self._arcs["attackable"][i] == 1:
-                vuln.append(self._arcs["v_init"][i] *
-                            np.exp(-gi * prevent_cost[len(self._nodes) + i]))
+                vuln.append(
+                    self._arcs["v_init"][i] * np.exp(-gi * self._prevent_cost[len(self._nodes) + i]))
             else:
                 vuln.append(0)
         return vuln
 
-    def consequence(self, response_cost):
+    def consequence(self):
         """Compute consequence function defining damages from a successful
         attack."""
         cons = []
         for i, bi in enumerate(self._beta(self._nodes)):
             if self._nodes["attackable"][i] == 1:
                 cons.append(self._nodes["c_init"][i] *
-                            np.exp(-bi * response_cost[i]))
+                            np.exp(-bi * self._response_cost[i]))
             else:
                 cons.append(0)
         for i, bi in enumerate(self._gamma(self._arcs)):
             if self._arcs["attackable"][i] == 1:
-                cons.append(self._arcs["c_init"][i] *
-                            np.exp(-bi * response_cost[len(self._nodes) + i]))
+                cons.append(
+                    self._arcs["c_init"][i] * np.exp(-bi * self._response_cost[len(self._nodes) + i]))
             else:
                 cons.append(0)
         return cons
+
+    def risk(self):
+        """Compute risk function."""
+        d_node = self._topology.node_degree_centrality()
+        d_arc = self._topology.arc_betweenness_centrality()
+        d = d_node + d_arc
+        t = self.threat()
+        v = self.vulnerability()
+        c = self.consequence()
+
+        z = np.zeros(len(d))
+        for i in range(len(d)):
+            z[i] = d[i] * t[i] * v[i] * c[i]
+
+        return z
