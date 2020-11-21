538ae0832cb3561529f55ee7c66b90a47e3ce02c
Sellevåg <Stig-Rune.Sellevag@ffi.no>
2020-11-21 11:48:49 +0100

    Updated stackelberg

--- snram/stackelberg.py
+++ snram/stackelberg.py
@@ -8,6 +8,7 @@
 
 from snram.attacker import Attacker
 from snram.defender import Defender
+from snram.network_risk import NetworkRisk
 from snram.risk_score import RISK_INC
 
 
@@ -20,7 +21,7 @@ def stackelberg(topology, budget=1, max_iter=10):
     print("                                                                      ")
     print("======================================================================")
     print()
-    print("Minimise Risk - Maximise Threat:")
+    print("Minimise Risk - Maximise Threat for Nodes:")
     print("%s" % ("-" * 70))
     print("#\tR_sum(V)\tR_sum(C)\tR_sum(T)")
     print("%s" % ("-" * 70))
@@ -28,15 +29,40 @@ def stackelberg(topology, budget=1, max_iter=10):
     for it in range(max_iter):
         # Minimise vulnerability:
         defender = Defender(topology, budget)
-        v_res, topology = defender.minimise_vulnerability()
+        v_res, topology = defender.minimise_vulnerability("nodes")
 
         # Minimise consequences:
         defender = Defender(topology, budget)
-        c_res, topology = defender.minimise_consequence()
+        c_res, topology = defender.minimise_consequence("nodes")
 
         # Maximise threat:
         attacker = Attacker(topology, budget)
-        t_res, topology = attacker.maximise_threat()
+        t_res, topology = attacker.maximise_threat("nodes")
+
+        print("%d\t%d\t\t%d\t\t%d" %
+              (it, v_res[-1][-1], c_res[-1][-1], t_res[-1][-1]))
+
+        if abs(t_res[-1][-1] - c_res[-1][-1]) <= RISK_INC:
+            break
+    print("%s" % ("-" * 70))
+
+    print("Minimise Risk - Maximise Threat for Links:")
+    print("%s" % ("-" * 70))
+    print("#\tR_sum(V)\tR_sum(C)\tR_sum(T)")
+    print("%s" % ("-" * 70))
+
+    for it in range(max_iter):
+        # Minimise vulnerability:
+        defender = Defender(topology, budget)
+        v_res, topology = defender.minimise_vulnerability("links")
+
+        # Minimise consequences:
+        defender = Defender(topology, budget)
+        c_res, topology = defender.minimise_consequence("links")
+
+        # Maximise threat:
+        attacker = Attacker(topology, budget)
+        t_res, topology = attacker.maximise_threat("links")
 
         print("%d\t%d\t\t%d\t\t%d" %
               (it, v_res[-1][-1], c_res[-1][-1], t_res[-1][-1]))
@@ -44,6 +70,8 @@ def stackelberg(topology, budget=1, max_iter=10):
         if abs(t_res[-1][-1] - c_res[-1][-1]) <= RISK_INC:
             break
     print("%s\n" % ("-" * 70))
-    topology.print()
-    topology.critical_asset_analysis()
+
+    network_risk = NetworkRisk(topology)
+    network_risk.risk_assessment()
+    network_risk.critical_assets()
     return topology
